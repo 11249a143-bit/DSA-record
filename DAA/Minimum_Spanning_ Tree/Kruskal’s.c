d9c3b7c4b07b64e3a3d40609c2d795fc298e1241
11249a143-bit <11249a143@kanchiuniv.ac.in>
2026-04-20 22:12:34 +0530

    Update Kruskal’s.c

--- DAA/Minimum_Spanning_ Tree/Kruskal’s.c
+++ DAA/Minimum_Spanning_ Tree/Kruskal’s.c
@@ -1,66 +1,99 @@
-#include <stdio.h> 
-#define V 5 
-#define E 6 
-int parent[V];  
-int findLeader(int v) 
-{ 
-    while (parent[v] != v) 
-        v = parent[v]; 
-    return v; 
-} 
-void mergeSets(int u, int v) 
-{ 
-    int a = findLeader(u); 
-    int b = findLeader(v); 
-    parent[a] = b; 
-} 
-void sortEdges(int edges[E][3]) 
-{ 
-    for (int i = 0; i < E - 1; i++) 
-    { 
-        for (int j = 0; j < E - i - 1; j++) 
-        { 
-            if (edges[j][2] > edges[j + 1][2])
-             { 
-                for (int k = 0; k < 3; k++) 
-                { 
-                    int temp = edges[j][k]; 
-                    edges[j][k] = edges[j + 1][k]; 
-                    edges[j + 1][k] = temp; 
-                } 
-            } 
-        } 
-    } 
-} 
-void kruskal(int edges[E][3]) 
-{ 
-    sortEdges(edges); 
-    for (int i = 0; i < V; i++) 
-        parent[i] = i; 
-    int count = 0, cost = 0; 
-    printf("Selected Edges:\n"); 
- 
-    for (int i = 0; i < E; i++) 
-    { 
-        int u = edges[i][0];   if (count == V - 1) 
-            break; 
-    } 
-    printf("Total Cost: %d\n", cost); 
-} 
-int main() 
-{ 
-    int edges[E][3] = { 
-        {0, 1, 2}, {0, 2, 3}, {1, 2, 5}, {1, 3, 3}, {2, 4, 4}, {3, 4, 2}}; 
- 
-    kruskal(edges); 
-    return 0; 
+/*
+Aim:
+To implement Kruskal’s algorithm to find the Minimum Spanning Tree (MST)
+of a weighted graph using a greedy approach.
+*/
+
+#include <stdio.h>
+
+#define V 5   // Number of vertices
+#define E 6   // Number of edges
+
+int parent[V];  // Disjoint set array
+
+// Function to find the representative (leader) of a set
+int findRoot(int vertex)
+{
+    while (parent[vertex] != vertex)
+    {
+        vertex = parent[vertex];
+    }
+    return vertex;
 }
-        int v = edges[i][1]; 
-        int w = edges[i][2]; 
-        if (findLeader(u) != findLeader(v)) 
-        { 
-            printf("%d -- %d (Weight: %d)\n", u, v, w); 
-            mergeSets(u, v); 
-            cost += w; 
-            count++; 
-        }   
+
+// Function to unite two sets
+void unionSets(int u, int v)
+{
+    int rootU = findRoot(u);
+    int rootV = findRoot(v);
+    parent[rootU] = rootV;
+}
+
+// Function to sort edges based on weight (Bubble Sort)
+void sortByWeight(int edgeList[E][3])
+{
+    for (int i = 0; i < E - 1; i++)
+    {
+        for (int j = 0; j < E - i - 1; j++)
+        {
+            if (edgeList[j][2] > edgeList[j + 1][2])
+            {
+                // Swap entire edge (u, v, w)
+                for (int k = 0; k < 3; k++)
+                {
+                    int temp = edgeList[j][k];
+                    edgeList[j][k] = edgeList[j + 1][k];
+                    edgeList[j + 1][k] = temp;
+                }
+            }
+        }
+    }
+}
+
+// Kruskal's algorithm implementation
+void applyKruskal(int edgeList[E][3])
+{
+    sortByWeight(edgeList);
+
+    // Initialize disjoint sets
+    for (int i = 0; i < V; i++)
+    {
+        parent[i] = i;
+    }
+
+    int edgeCount = 0;
+    int totalCost = 0;
+
+    printf("Edges selected for MST:\n");
+
+    for (int i = 0; i < E && edgeCount < V - 1; i++)
+    {
+        int u = edgeList[i][0];
+        int v = edgeList[i][1];
+        int weight = edgeList[i][2];
+
+        // Check if adding this edge forms a cycle
+        if (findRoot(u) != findRoot(v))
+        {
+            printf("%d -- %d  (Weight: %d)\n", u, v, weight);
+            unionSets(u, v);
+            totalCost += weight;
+            edgeCount++;
+        }
+    }
+
+    printf("Total cost of MST: %d\n", totalCost);
+}
+
+int main()
+{
+    // Edge format: {source, destination, weight}
+    int edges[E][3] = {
+        {0, 1, 2}, {0, 2, 3}, {1, 2, 5},
+        {1, 3, 3}, {2, 4, 4}, {3, 4, 2}
+    };
+
+    applyKruskal(edges);
+
+    return 0;
+}
