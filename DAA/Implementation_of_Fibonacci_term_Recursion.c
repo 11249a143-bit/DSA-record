e27db3c48e810789a4fdc87a7f2e8710169fd034
11249a143-bit <11249a143@kanchiuniv.ac.in>
2026-04-20 22:15:54 +0530

    Update Implementation_of_Fibonacci_term_Recursion.c

--- DAA/Implementation_of_Fibonacci_term_Recursion.c
+++ DAA/Implementation_of_Fibonacci_term_Recursion.c
@@ -1,19 +1,36 @@
-#include<stdio.h>
-int fib(int n) 
-{   
-  if (n <= 1) 
-  { 
-    return n; 
-  }   
-  return fib(n - 1) + fib(n - 2); 
+/*
+Aim:
+To find the nth Fibonacci number using recursion.
+*/
+
+#include <stdio.h>
+
+// Recursive function to compute Fibonacci
+int fibonacci(int n)
+{
+    if (n <= 1)
+        return n;
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
-int main()  { 
-  int n, result = 0;  
-  if (n != 1 || n < 0) 
-  { 
-    printf("Invalid input\n"); 
-    return 1; 
-  } 
-  printf("\nNth Fibonacci term = %d\n", fib(n)); 
-  return 0; 
-} 
+
+int main()
+{
+    int n;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    // Input validation
+    if (n < 0)
+    {
+        printf("Invalid input. Please enter a non-negative integer.\n");
+        return 1;
+    }
+
+    int result = fibonacci(n);
+
+    printf("Nth Fibonacci term = %d\n", result);
+
+    return 0;
+}
